91fa8040d20d36139eaf162c6c59b3d449f15c68
kaizen857 <xieweili2005@foxmail.com>
2024-11-22 17:38:15 +0800

    test

--- test9/includes/huff.hpp
+++ test9/includes/huff.hpp
@@ -16,6 +16,12 @@ public:
         std::shared_ptr<Node> mright;
 
     public:
+        // 作为超节点
+        Node(int frequency) :
+            mdata('\0'), mfrequency(frequency), mleft(nullptr), mright(nullptr) {}
+        Node(int frequency, std::shared_ptr<Node> left, std::shared_ptr<Node> right) :
+            mdata('\0'), mfrequency(frequency), mleft(left), mright(right) {}
+        // 作为叶子节点
         Node(char data, int frequency) :
             mdata(data), mfrequency(frequency), mleft(nullptr), mright(nullptr) {}
         Node(char data, int frequency, std::shared_ptr<Node> left, std::shared_ptr<Node> right) :
@@ -27,6 +33,7 @@ public:
         std::shared_ptr<Node> getRight();
         void setLeft(std::shared_ptr<Node> left);
         void setRight(std::shared_ptr<Node> right);
+        friend class HuffmanTree;
     };
     const std::shared_ptr<Node> getRoot() const
     {
@@ -34,11 +41,16 @@ public:
     }
     HuffmanTree(std::vector<std::shared_ptr<Node>> nodes);
     ~HuffmanTree() { root.reset(); }
-    void printTree(std::shared_ptr<Node> node, std::string code = ""); // Helper function to print the tree
+    void printTree(std::shared_ptr<Node> node, std::string code = "");
+    std::string encode(const std::string &text);
+    std::string decode(const std::string &encodedText);
 
 private:
     std::shared_ptr<Node> root;
-    std::shared_ptr<Node> buildTree(std::vector<std::shared_ptr<Node>> nodes);
+    void clear()
+    {
+        root.reset();
+    }
 };
 
 #endif
